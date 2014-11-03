087af4da3c7ac9dbafb3dfd30af3e5c5f2f0b758
Anton Karamanov <ataraxer@gmail.com>
2014-11-03 23:05:04 +0300

    Move to SASS for stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
 var minifyHTML = require('gulp-minify-html');
 var minifyCSS = require('gulp-minify-css');
+var sass = require('gulp-sass')
 
 
 gulp.task('default', ['build'])
 
 gulp.task('build', function () {
-  gulp.src('src/styles/*.css')
+  gulp.src('src/styles/*.scss')
+    .pipe(sass())
     .pipe(minifyCSS())
     .pipe(gulp.dest('build/styles'));
 
