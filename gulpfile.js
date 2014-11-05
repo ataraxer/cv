12d4355860351802ee727ecc5669a1f2d6a16ef4
Anton Karamanov <ataraxer@gmail.com>
2014-11-05 23:31:21 +0300

    Use Bower to load resources instead of CDN

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var minifyHTML = require('gulp-minify-html');
 var minifyCSS = require('gulp-minify-css');
 var sass = require('gulp-sass')
 var assemble = require('gulp-assemble');
+var bower = require('gulp-bower');
 
 var assembleOptions = {
   data: 'src/data.json',
@@ -11,13 +12,24 @@ var assembleOptions = {
   helpers: 'src/templates/helpers.js'
 };
 
+var pureFiles = [
+  'bower_components/pure/pure-min.css',
+  'bower_components/pure/grids-responsive-min.css',
+];
+
+
 
 gulp.task('default', ['build'])
 
 gulp.task('build', function () {
+  bower();
+
   gulp.src('src/styles/*.scss')
     .pipe(sass())
     .pipe(minifyCSS())
+    .pipe(gulp.dest('build/styles'));
+
+  gulp.src(pureFiles)
     .pipe(gulp.dest('build/styles'));
 
   gulp.src('src/templates/*.hbs')
